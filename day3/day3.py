dbf330af1d438ff62068a9df9f2ef3868b208e83
David Skerritt <irishsaltwater@gmail.com>
2024-12-08 14:44:53 +0000

    Day 3: Part 2

--- day3/day3.py
+++ day3/day3.py
@@ -28,4 +28,46 @@ def part_one():
 
     print("Result: " + str(total_result))
 
-part_one()
+def part_two():
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    
+    # do() - start taking account of muls after that
+    # don't() - ignore following muls until do
+
+    # so, ah- if going through input in order, just append in order
+    # match mul, or do or don't
+
+    # then when iterating, step each one, either calc, or flip boolean if do or dont
+    do_raw= "do()"
+    dont_raw = "don't()"
+    do_regex = 'do\(\)'
+    dont_regex = "don't\(\)"
+    command_regex = 'mul\([0-9]{1,3},[0-9]{1,3}\)' # finds mul(1-3digits,1-3digits)
+    
+    full_regex = do_regex + '|' + dont_regex + '|' + command_regex
+
+    matches = []
+
+    with open(os.path.join(__location__, 'inputs.csv')) as input:
+        for line in input:
+            matches += re.findall(full_regex, line)
+    print("Matches found, size: " + str(len(matches)))
+
+    # Parse each command
+    total_result = 0
+    
+    enabled = True
+
+    for command in matches:
+        if command == do_raw:
+            enabled = True
+        elif command == dont_raw:
+            enabled = False
+        elif enabled: # At this point, know command is a multiplication
+            total_result += parse_multiplication(command)
+
+    print("Result: " + str(total_result))
+
+# part_one() # 178886550
+# part_two() # 87163705
