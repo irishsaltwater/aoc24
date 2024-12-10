4d370d2d8b6182a4db4a123e07476629e577954d
David Skerritt <irishsaltwater@gmail.com>
2024-12-10 08:34:09 +0000

    Day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -69,4 +69,63 @@ def part_one():
                             
     print("Word found count: " + str(word_count)) # 2549
 
-part_one()
+
+def part_two():
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    
+    word_search = []
+
+    # build 2 array
+    with open(os.path.join(__location__, 'input.txt')) as input:
+        for line in input:
+            word_search.append(list(line))
+
+    # Need to find X-MAS, ie MAS in shape of X
+    # Find A, then just check four corners
+    # Top right, bottom left must be MS
+    # Top left, bottom right must be MS
+
+    middle = 'A'
+
+    find_count = 0
+
+    for y, line in enumerate(word_search):
+        for x, character in enumerate(line):
+            if character == middle:
+                location_yx = (y,x)
+                # Check diagonals for MS
+                top_left_loc = numpy.add(location_yx, up_left)
+                bottom_right_loc = numpy.add(location_yx, down_right)
+
+                top_right_loc = numpy.add(location_yx, up_right)
+                bottom_left_loc = numpy.add(location_yx, down_left)
+
+                diagonals = ((top_left_loc, bottom_right_loc),(top_right_loc, bottom_left_loc))
+
+                # Diagonal contains two tuples
+                diagonal_found_count = 0
+                for diagonal in diagonals:
+                    letters = "MS"
+
+                    for loc in diagonal:
+                        if loc[1] > (len(line) - 1) or loc[1] < 0:
+                                # outside bounds, stop checking this direction
+                                break
+                        if loc[0] > (len(word_search) - 1) or loc[0] < 0:
+                            # outside bounds, stop checking
+                            break
+                        # Replace letter with blank space, if no match then letter not replaced
+                        letters = letters.replace(word_search[loc[0]][loc[1]], '')
+                    
+                    if len(letters) == 0:
+                        # If both letters replaced, then know this diagonal contains needed letters
+                        diagonal_found_count += 1
+                
+                if diagonal_found_count == 2:
+                    find_count += 1
+                            
+    print("X-MAS found count: " + str(find_count)) # 2003
+
+#part_one()
+part_two()
